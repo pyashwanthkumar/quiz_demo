83e5ac86bf10b8e316dfc841c3b390ebcb51a437
Yashwanth Kumar Pamidimukkala <ykp3@gatech.edu>
2022-06-22 19:14:59 -0400

    app done

--- src/pages/FinalScreen.js
+++ src/pages/FinalScreen.js
@@ -1,12 +1,26 @@
+import { Button, Typography } from '@mui/material'
+import { Box } from '@mui/system'
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { handleNumberChange, handleScoreChange } from '../redux/quiz'
 
 const FinalScreen = () => {
   const {score} = useSelector(state => state.quiz)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const handleBackToSettings = () => {
+    dispatch(handleScoreChange(0))
+    dispatch(handleNumberChange(50))
+    navigate("/")
+  }
 
   return (
-    <Box>
-        
+    <Box mt={30}>
+        <Typography variant="h3" fontWeight="bold" mb={3}>
+          Final Score : {score}
+        </Typography>
+        <Button onClick={handleBackToSettings} variant="outlined">back to settings</Button>
     </Box>
   )
 }
